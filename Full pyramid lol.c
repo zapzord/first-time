281cac7639f39653e6e306378a879bc34dc0d052
zapzord <75105560+zapzord@users.noreply.github.com>
2021-05-18 11:14:57 +0530

    Create Full pyramid lol.c

---
+++ Full pyramid lol.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c,d;
+printf("enter rows: ");
+scanf("%d",&a);
+for(b=1;b<=a;b++)
+ {for(c=1;c<=a-b;c++)
+   {printf(" ");}
+for(d=1;d<=b;d++)
+ {printf("* ");}
+printf("\n");
+}}
